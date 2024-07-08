a45d48b01391d370865e363696fa7f2bf144d628
Glailtonnascimento <glailtonnascimento88@gmail.com>
2024-07-08 15:49:13 -0300

    Santander-dev-week-Glailton-2024

---
+++ src/main/java/me/dio/Glailton/Santander_dev_week_Glailton_2024/Application.java
@@ -0,0 +1,13 @@
+package me.dio.Glailton.Santander_dev_week_Glailton_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/Glailton/Santander_dev_week_Glailton_2024/SantanderDevWeekGlailton2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Glailton.Santander_dev_week_Glailton_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeekGlailton2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
